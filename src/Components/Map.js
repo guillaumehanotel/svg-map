39ef85e695d29d01172a685fb6c52b6b01688e1e
guillaumehanotel <guillaumehanotel@orange.fr>
2021-12-19 23:32:19 +0100

    begin front logic

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,70 +1,131 @@
-import React, {useState}  from 'react';
-import PropTypes from 'prop-types';
+import React, {useEffect, useState} from 'react';
 import {SVGMap, CheckboxSVGMap} from "react-svg-map";
 import "react-svg-map/lib/index.css";
-import {Link, Outlet, useParams} from "react-router-dom";
-import {getLocationId, getLocationName, slugify} from "../utils";
+import {Link, useParams} from "react-router-dom";
+import {getLocationId, getLocationName} from "../utils";
 import {getMapBySlug} from "../data";
 
-
-function Map(props) {
+function Map() {
 
     const params = useParams();
     const map = getMapBySlug(params.mapSlug);
+    const mapLocations = (map.locations.map(location => location.name));
+    const MAX_TRIES = 3;
+    const defaultTooptipStyle = {
+        display: 'none'
+    };
 
-    const [pointedLocation, setPointedLocation] = useState(null);
     const [selectedLocation, setSelectedLocation] = useState(null);
-    const [selectedLocations, setSelectedLocations] = useState([]);
+    const [locations, setLocations] = useState(
+        Object.fromEntries(mapLocations.map(mapLocation => [mapLocation, false]))
+    );
+    const [requestedLocation, setRequestedLocation] = useState(null)
+    const [tries, setTries] = useState(0);
+    const [score, setScore] = useState(0);
+    const [tooltipStyle, setTooltypeStyle] = useState(defaultTooptipStyle);
+    
+    const getLocationIdByLocationName = (name) => map.locations
+            .filter(location => location.name === name)
+            .map(location => location.id)[0]
 
-    const handleLocationMouseOver = (event) => {
-        setPointedLocation(getLocationName(event));
+    const getUncheckedLocations = () => Object.entries(locations)
+        .filter(location => !location[1])
+        .map(location => location[0])
+
+    const getCheckedLocations = () => Object.entries(locations)
+        .filter(location => location[1])
+        .map(location => location[0])
+
+    const getRandomUncheckedLocations = () => {
+        const uncheckedLocations = getUncheckedLocations();
+        return uncheckedLocations[Math.floor(Math.random() * uncheckedLocations.length)];
     }
 
-    const handleLocationMouseOut = () => {
-        setPointedLocation(null);
+    const displayTooltip = (event, isSuccess) => {
+        const tooptipStyle = {
+            display: 'block',
+            top: event.clientY + 10,
+            left: event.clientX - 100,
+            backgroundColor : isSuccess ? 'green' : 'red'
+        };
+        setTooltypeStyle(tooptipStyle);
+        setTimeout(() => {
+            setTooltypeStyle(defaultTooptipStyle);
+        }, 500)
     }
 
     const handleLocationClick = (event) => {
         const clickedLocation = getLocationName(event);
+
+        const requestedLocationId = getLocationIdByLocationName(requestedLocation);
+
+        const isSuccess = clickedLocation === requestedLocation;
+
+        if (isSuccess) {
+            
+            setScore(prevState => prevState + 10);
+            setLocations(prevState => ({
+                ...prevState,
+                [clickedLocation]: true
+            }))
+            document.querySelector(`#${requestedLocationId}`).classList.add('found');
+            setTries(0);
+            setRequestedLocation(getRandomUncheckedLocations());
+            
+        } else {
+            setTries(prevState => prevState + 1);
+            if (tries + 1 === MAX_TRIES) {
+                document.querySelector(`#${requestedLocationId}`).classList.add('fail')
+                setLocations(prevState => ({
+                    ...prevState,
+                    [requestedLocation]: true
+                }))
+                setRequestedLocation(getRandomUncheckedLocations());
+                setTries(0);
+            }
+            setScore(prevState => prevState - 5);
+        }
+        displayTooltip(event, isSuccess);
         setSelectedLocation(clickedLocation);
     }
 
-    const handleOnChange = (locations) => {
-        const locationNames = locations.map((location) => location.ariaLabel);
-        setSelectedLocations(locationNames);
-    }
 
-    const MapLocations = () => (
-            <ul>
-                {map.locations.map((location) => (<li key={location.id}>{location.name}</li>))}
-            </ul>
-        )
+
+    useEffect(() => {
+        setRequestedLocation(getRandomUncheckedLocations());
+    }, []);
+
+    // const handleOnChange = (locations) => {
+    //     const locationNames = locations.map((location) => location.ariaLabel);
+    //     setLocations(locationNames);
+    // }
 
     return (
         <div className={"map"}>
 
             <Link className={"home-link"} to={"/"}>Home</Link>
 
-
             <div className="examples__block__info">
-                <div className="examples__block__info__item">
-                    Pointed location: {pointedLocation}
-                </div>
+                <p>Score : {score}</p>
+                <p>Requested Location : {requestedLocation} ({tries}/{MAX_TRIES})</p>
                 <div className="examples__block__info__item">
                     Clicked location: {selectedLocation}
                 </div>
+                {/*<div className="examples__block__info__item">*/}
+                {/*    Done Locations: {getCheckedLocations()}*/}
+                {/*</div>*/}
                 <div className="examples__block__info__item">
-                    Selected locations: {selectedLocations}
+                    Total : {getCheckedLocations().length} / {mapLocations.length}
                 </div>
             </div>
-            <CheckboxSVGMap
+            <SVGMap
                 map={map}
-                onLocationMouseOver={handleLocationMouseOver}
-                onLocationMouseOut={handleLocationMouseOut}
-                // onLocationClick={handleLocationClick}
-                onChange={handleOnChange}
+                onLocationClick={handleLocationClick}
+                // onChange={handleOnChange}
             />
-            {/*<MapLocations />*/}
+            <div className="map-location-tooltip" style={tooltipStyle}>
+                {selectedLocation}
+            </div>
         </div>
 
     );
